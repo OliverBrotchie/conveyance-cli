14dadb3329512b9722c9ce38ce8b6e8a6b4d0e46
Oliver Brotchie <o.brotchie@gmail.com>
2021-08-02 17:21:44 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use zip::write::{FileOptions, ZipWriter};
 
 /// Arguments
 #[derive(StructOpt, Debug)]
-#[structopt(name = "basic")]
+#[structopt(name = "Conveyancing CLI")]
 struct Args {
     /// The path/s to the desired word file/s.
     #[structopt(short, long, parse(from_os_str))]
